5b529853091d5cadca6cf9c05e49a4e39efdac87
deepakcode21@gmail.com <deepakcode21@gmail.com>
2025-06-16 23:49:45 +0530

    E1

--- school/src/components/ui/sidebar.jsx
+++ school/src/components/ui/sidebar.jsx
@@ -7,7 +7,7 @@ import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/Separator"
 import { Sheet, SheetContent } from "@/components/ui/sheet"
 import { Skeleton } from "@/components/ui/skeleton"
 import {
